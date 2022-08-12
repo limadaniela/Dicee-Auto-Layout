b143581558ccae2b27450190fb4fa8a0dd73072b
limadaniela <62920069+limadaniela@users.noreply.github.com>
2022-08-11 20:35:24 -0600

    Initial release

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Daniela Lima on 2022-04-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+    }
+    
+}
+
